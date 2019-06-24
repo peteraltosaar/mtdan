bd98ce02e3acc8654d650f3716d8b2472c94b30a
Peter Altosaar <peter.altosaar@gmail.com>
2019-06-23 23:37:29 -0400

    De-duplicate creation of Todoist HTTP headers

--- src/main/java/com/altocorp/mtdan/web/TodoService.java
+++ src/main/java/com/altocorp/mtdan/web/TodoService.java
@@ -23,26 +23,27 @@ public class TodoService {
     }
 
     List<TodoistTodo> getTodos() {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("Authorization", "Bearer " + todoistBearerToken);
-        HttpEntity httpEntity = new HttpEntity(httpHeaders);
+        HttpEntity httpEntity = createTodoistHttpEntity();
         ResponseEntity<List<TodoistTodo>> todosEntity = restTemplate.exchange("https://beta.todoist.com/API/v8/tasks", HttpMethod.GET, httpEntity, new ParameterizedTypeReference<List<TodoistTodo>>() {});
         return todosEntity.getBody();
     }
 
     List<TodoistProject> getProjects() {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("Authorization", "Bearer " + todoistBearerToken);
-        HttpEntity httpEntity = new HttpEntity(httpHeaders);
+        HttpEntity httpEntity = createTodoistHttpEntity();
         ResponseEntity<List<TodoistProject>> projectsEntity = restTemplate.exchange("https://beta.todoist.com/API/v8/projects", HttpMethod.GET, httpEntity, new ParameterizedTypeReference<List<TodoistProject>>() {});
         return projectsEntity.getBody();
     }
 
     List<TodoistLabel> getLabels() {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("Authorization", "Bearer " + todoistBearerToken);
-        HttpEntity httpEntity = new HttpEntity(httpHeaders);
+        HttpEntity httpEntity = createTodoistHttpEntity();
         ResponseEntity<List<TodoistLabel>> projectsEntity = restTemplate.exchange("https://beta.todoist.com/API/v8/labels", HttpMethod.GET, httpEntity, new ParameterizedTypeReference<List<TodoistLabel>>() {});
         return projectsEntity.getBody();
     }
+
+    private HttpEntity createTodoistHttpEntity() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization", "Bearer " + todoistBearerToken);
+        return new HttpEntity(httpHeaders);
+    }
+
 }
